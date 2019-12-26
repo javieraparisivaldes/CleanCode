0a1fb12ce0bb5820bcd7dbb817dea9ceaa53b927
japarisi <japarisivaldes@gmail.com>
2019-12-26 07:10:52 +0100

    add variables and functions examples

---
+++ functions.js
@@ -0,0 +1,7 @@
+//< 4 parameters
+const getPersons = (age, male, weight) =>{
+}
+
+//>= 4 parameters
+const getPersons = ({age, male, weight, religion}) => {
+}
--- variables.js
+++ variables.js
@@ -1,6 +1,23 @@
-//tipo array
-//mal
+//Array
+//bad
 let userList = [];
 
-//bien
+//good
 let users = [];
+
+//Bool
+//bad
+let open = false;
+let fruit = false;
+
+//good
+let isOpen = false;
+let hasFruit = false;
+
+//Numbers
+//bad
+const bananas = 7;
+
+//good
+const totalBananas = 7;
+const minBananas = 2;
